cabe59d3b92595147f308db2818c054cf6140b4e
JonasKlare <JonasKlare@users.noreply.github.com>
2017-02-18 21:30:48 -0600

    Add files via upload

---
+++ Assets/RotationsScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationsScript : MonoBehaviour {
+
+    public Rigidbody2D rb;
+    public float _speed = 1.0f;
+    public float rotation;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rotation = rotation + _speed + Time.fixedDeltaTime;
+        rb.MoveRotation(rotation);
+        
+    }
+}
---
+++ Assets/cameraMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMovement : MonoBehaviour {
+
+    public float speed = 1f;
+    public float bloodStreamSpeed = 1f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+        transform.position += Vector3.down * speed * Time.deltaTime;
+    }
+}
