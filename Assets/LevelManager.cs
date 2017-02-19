93823c9421e2990efd5d8e313c86125b2228c23f
Carter Steffen <Carter Steffen>
2017-02-19 04:07:35 -0600

    Level Menus

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    public Transform MainMenu, WinMenu, LoseMenu;
+
+    public void LoadScene(string name)
+    {
+        Application.LoadLevel(name);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void winnerLevel(bool clicked)
+    {
+        if(clicked == true)
+        {
+            MainMenu.gameObject.SetActive(clicked);
+            WinMenu.gameObject.SetActive(false);
+        } else
+        {
+            MainMenu.gameObject.SetActive(clicked);
+            WinMenu.gameObject.SetActive(true);
+        }
+    }
+
+    public void loseLevel(bool clicked)
+    {
+        if (clicked == true)
+        {
+            MainMenu.gameObject.SetActive(clicked);
+            LoseMenu.gameObject.SetActive(false);
+        }
+        else
+        {
+            MainMenu.gameObject.SetActive(clicked);
+            LoseMenu.gameObject.SetActive(true);
+        }
+    }
+}
